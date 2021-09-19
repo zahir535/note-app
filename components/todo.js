70743adf5e552f69aade8b58c34868c9a6f18597
zahir535 <90959795+zahir535@users.noreply.github.com>
2021-09-19 23:25:44 +0800

    Add files via upload

--- components/todo.js
+++ components/todo.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity,ScrollView } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function TodoItem({item, pressHandler}) {
     
     
     return(
-        <TouchableOpacity style={styles.container} onPress={ () => pressHandler(item.key) } >
-            <Text> {item.text} </Text>
+        //original code, touchable to delete on the box itself
+        // <TouchableOpacity style={styles.container} onPress={ () => pressHandler(item.key) } >
+        //     <View style={styles.item}>                
+        //         <Text> {item.text} </Text>
+        //         <AntDesign name='close' size={24} color='black'  />
+        //     </View>         
+        // </TouchableOpacity>
+
+        //touchable to delete on the icon
+        //TouchableOpacity can be replaced by Scrollview. But its not smooth responsive like TouchableOpacity
+        <TouchableOpacity style={styles.container}>
+            <View style={styles.item}>
+                <Text>{item.text}</Text>
+                <TouchableOpacity onPress={ ()=>pressHandler(item.key) } >
+                    <AntDesign name='close' size={24} color='black' />
+                </TouchableOpacity>
+            </View>
         </TouchableOpacity>
+
     );
 }
 
@@ -21,5 +37,12 @@ const styles = StyleSheet.create({
         borderColor: "#333",
         borderStyle: 'solid',
         borderRadius: 10,
+        // backgroundColor: 'yellow',
+    },
+    item:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        // backgroundColor: '#fff',
     }
 });
